0f638e1a552ae57e3fb5286a4e487067dcbaa637
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2020-08-01 07:13:12 -0300

    init

---
+++ source/index.ts
@@ -0,0 +1,9 @@
+import { PostgresInfo } from './database/postgresInfo';
+import { PostgresDB } from './postgresDB';
+// import { PostgresDB } from './adapter';
+// import { PostgresDB } from './dAO';
+// import { PostgresDB } from './service';
+export {
+  PostgresInfo,
+  PostgresDB
+};
